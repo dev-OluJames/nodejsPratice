df95c5ea6601a0e12bdf43904c200a7a16247fa9
dev James <adeolajames93@gmail.com>
2021-02-20 19:45:06 +0100

    nodejs callback

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-let rectangle = require('./rectangle')
+let rect = require('./rectangle')
 
 let solveRectangle = function (l,b) {
-    if(l<=0 || b<=0){
-        console.log("the value of l and b must be greater than 0. l=",l,"b=",b)
-    }
-    else{
-        console.log("the perimeter =", rectangle.perimeter(l,b))
-        console.log("the area =", rectangle.area(l,b))
-    }
+    console.log("Proceding to the perimeter and area computing. l: ",l," b: ",b)
+    rect(l, b, (err, rectangle) => {
+        if(err){
+            console.log(err.message)
+        }
+        else {
+            console.log("the perimeter =", rectangle.perimeter(l, b))
+            console.log("the area =", rectangle.area(l, b))
+        }
 
+    })
+    console.log("hello. l: ",l," b: ",b)
 }
-console.log("hello")
+
 solveRectangle(3,4)
 solveRectangle(0,4)
--- rectangle.js
+++ rectangle.js
@@ -1,6 +1,16 @@
-let rectangle = {
-    perimeter: (x,y)=>(2*(x+y)),
-    area : (x,y) =>(x*y)
-}
 
-module.exports = rectangle
+module.exports = (x,y,callBack) => {
+    if(x<=0 || y<=0 ){
+        setTimeout(()=>{
+            callBack(new Error("error"),null)
+        }, 2000);
+    }else{
+        setTimeout(()=>{
+            callBack(null, {
+                perimeter: ()=>(2*(x+y)),
+                area : () =>(x*y)
+            })
+        }, 2000);
+    }
+
+}
